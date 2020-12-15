b4ad9a6415a558df501e75665f41e471d3d5081a
Çılga İşcan <cilgaiscan@std.iyte.edu.tr>
2020-12-15 20:59:05 +0300

    Add doty.app logo to navbar component.

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -3,9 +3,12 @@
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
+import logo from "@/assets/logo.png";
 
 @Component({
   name: 'Navbar',
 })
-export default class Navbar extends Vue {}
+export default class Navbar extends Vue {
+  private logo: any = logo;
+}
 </script>
